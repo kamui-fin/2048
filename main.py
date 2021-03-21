019f73d6ae62ca10de6292620936cde3fd50631c
Kamui <fin-kamui@pm.me>
2021-03-21 10:06:59 -0500

    format main file

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
 import pygame
 from pygame.locals import *
 import random
+
 S_HEIGHT = 600 + 125
 S_WIDTH = 600 + 125 + 50
 BOARD_HEIGHT = 4
 BOARD_WIDTH = 4
-screen = pygame.display.set_mode((S_HEIGHT, S_WIDTH))
-pygame.init()
-pygame.font.init()
-pygame.display.set_caption("h")
 BACKGROUND_COLOR_GAME = "#92877d"
 BACKGROUND_COLOR_CELL_EMPTY = "#9e948a"
+
 BACKGROUND_COLOR_DICT = {2: "#eee4da", 4: "#ede0c8", 8: "#f2b179",
                          16: "#f59563", 32: "#f67c5f", 64: "#f65e3b",
                          128: "#edcf72", 256: "#edcc61", 512: "#edc850",
                          1024: "#edc53f", 2048: "#edc22e",
                          4096: "#eee4da", 8192: "#edc22e", 16384: "#f2b179",
                          32768: "#f59563", 65536: "#f67c5f", }
+
 CELL_COLOR_DICT = {2: "#776e65", 4: "#776e65", 8: "#f9f6f2", 16: "#f9f6f2",
                    32: "#f9f6f2", 64: "#f9f6f2", 128: "#f9f6f2",
                    256: "#f9f6f2", 512: "#f9f6f2", 1024: "#f9f6f2",
                    2048: "#f9f6f2",
                    4096: "#776e65", 8192: "#f9f6f2", 16384: "#776e65",
                    32768: "#776e65", 65536: "#f9f6f2", }
-font = pygame.font.SysFont("Verdana", 50, True)
+
+screen = pygame.display.set_mode((S_HEIGHT, S_WIDTH))
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption("h")
+font = pygame.font.SysFont("DejaVu Sans Mono Nerd Font Mono", 52, True)
+
 class Cell:
     def __init__(self, rect, value):
         self.rect = rect
         self.value = value
         self.refresh_colors()
+
     def refresh_colors(self):
         if self.value:
             self.fg = pygame.Color(CELL_COLOR_DICT[self.value])
@@ -36,10 +42,12 @@ class Cell:
         else:
             self.fg = ""
             self.bg = pygame.Color(BACKGROUND_COLOR_CELL_EMPTY)
+
 class Board:
     def __init__(self):
         self._board = [[Cell("", 0) for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
         self.score = 0
+
     def init_board(self):
         left_offset = 25
         top_offset = 25
@@ -59,6 +67,7 @@ class Board:
                 random_h = random.randint(0, BOARD_WIDTH - 1)
             rand_val =  random.choices([2,4],weights=[.85,.15],k=1)[0]
             self._board[random_h][random_w].value = rand_val
+
     def draw_board(self):
         global font
         for row in self._board:
@@ -89,6 +98,7 @@ class Board:
                         else:
                             next_block.value = curr_block.value
                         curr_block.value = 0
+
     def refresh(self):
         for row in self._board:
             for cell in row:
@@ -101,6 +111,7 @@ left_info = {"col_range":(0,4,1),"row_range":(0,4,1), "col_int": -1, "row_int":0
 up_info = {"col_range":(0,4,1),"row_range":(0,4,1), "col_int": 0, "row_int":-1, "num_limit":0, "col_limit":-1}
 right_info = {"col_range":(3,-1,-1),"row_range":(0,4,1), "col_int": 1, "row_int":0, "num_limit":-1, "col_limit":3}
 down_info = {"col_range":(0,4,1),"row_range":(3,-1,-1), "col_int": 0, "row_int":1, "num_limit":3, "col_limit":-1}
+
 while run:
     clock.tick(60)
     for event in pygame.event.get():
@@ -117,8 +128,10 @@ while run:
                 board.move_board(**down_info)
             if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
                 board.spawn_cell()
+
     board.refresh()
     screen.fill(pygame.Color(BACKGROUND_COLOR_GAME))
     board.draw_board()
     pygame.display.flip()
+
 pygame.quit()
